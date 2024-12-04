e417e8b9cbd6c5ea18de1f5db398cd0db8e33f48
feivegian <feivegian@gmail.com>
2024-12-04 13:19:53 +0800

    feat: Changes to about screen

--- app/src/main/java/io/github/feivegian/music/fragments/AboutFragment.kt
+++ app/src/main/java/io/github/feivegian/music/fragments/AboutFragment.kt
@@ -1,7 +1,9 @@
 package io.github.feivegian.music.fragments
 
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.content.edit
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
@@ -11,15 +13,19 @@ import io.github.feivegian.music.activities.PreferenceActivity
 
 class AboutFragment : PreferenceFragmentCompat() {
     private lateinit var activity: PreferenceActivity
+    private lateinit var customTabsIntent: CustomTabsIntent
 
     private var uec = 0
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preference_about, rootKey)
         activity = requireActivity() as PreferenceActivity
-        val name = findPreference<Preference>("name")
+        customTabsIntent = CustomTabsIntent.Builder().build()
+        val name = findPreference<Preference>("about_name")
+        val website = findPreference<Preference>("project_website")
+        val source = findPreference<Preference>("project_source")
 
-        name?.summary = getString(R.string.preference_about_version, BuildConfig.VERSION_NAME)
+        name?.summary = getString(R.string.preference_about_version, BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE)
         name?.setOnPreferenceClickListener {
             // "uec" and "ex" is named here because we want this kind of functionality to be hidden
             // once the user has unlocked this, experiments will be accessible
@@ -41,6 +47,16 @@ class AboutFragment : PreferenceFragmentCompat() {
                 uec = 0 // reset to zero after unlocking experiments
             }
 
+            true
+        }
+        website?.setOnPreferenceClickListener {
+            val url = getString(R.string.preference_project_website_summary)
+            customTabsIntent.launchUrl(requireContext(), Uri.parse(url))
+            true
+        }
+        source?.setOnPreferenceClickListener {
+            val url = it.summary.toString()
+            customTabsIntent.launchUrl(requireContext(), Uri.parse(url))
             true
         }
     }
