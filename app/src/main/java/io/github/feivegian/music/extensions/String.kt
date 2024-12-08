005189b60bfa83b9cef43087f803935a5dd0dd62
feivegian <feivegian@gmail.com>
2024-12-08 20:40:14 +0800

    docs: Add documentation for String.kt

--- app/src/main/java/io/github/feivegian/music/extensions/String.kt
+++ app/src/main/java/io/github/feivegian/music/extensions/String.kt
@@ -2,6 +2,11 @@ package io.github.feivegian.music.extensions
 
 import android.util.Patterns
 
+/**
+ * Checks whether the String is a valid web URL.
+ *
+ * @return[Boolean]
+ */
 fun String.isWebUrl(): Boolean {
     return Patterns.WEB_URL.matcher(this).matches()
 }
