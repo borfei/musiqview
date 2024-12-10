a3dfa8e2d8b3b5a8f3264b2e722673c8a6cf05cd
feivegian <feivegian@gmail.com>
2024-12-10 21:18:53 +0800

    Bump version to 3.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.feivegian.music"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "3.4.0"
+        versionCode = 6
+        versionName = "3.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
