7cd6ddffe92ebde16d13a5ba158d9e3e93c75e8f
feivegian <feivegian@gmail.com>
2024-12-11 20:23:41 +0800

    feat: Implement ability to show/hide app from launcher in MainActivity

--- app/src/main/java/io/github/feivegian/music/activities/MainActivity.kt
+++ app/src/main/java/io/github/feivegian/music/activities/MainActivity.kt
@@ -1,8 +1,12 @@
 package io.github.feivegian.music.activities
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import io.github.feivegian.music.App.Companion.asApp
 import io.github.feivegian.music.R
 import io.github.feivegian.music.databinding.ActivityMainBinding
 import io.github.feivegian.music.extensions.adjustPaddingForSystemBarInsets
@@ -10,6 +14,7 @@ import io.github.feivegian.music.extensions.isActivityEnabled
 import io.github.feivegian.music.extensions.setActivityEnabled
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var preferences: SharedPreferences
     private lateinit var binding: ActivityMainBinding
 
     private var disableOnDestroy: Boolean = false
@@ -18,16 +23,33 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
+        // Get the preference instance from App
+        preferences = application.asApp().getPreferences()
+
+        // If "welcome_hide_launcher" is true, launch PreferenceActivity instead
+        if (!preferences.getBoolean("welcome_hide_launcher", true)) {
+            startActivity(Intent(this, PreferenceActivity::class.java))
+            finish()
+        }
+
         // Inflate activity view using ViewBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         binding.root.adjustPaddingForSystemBarInsets(top=true, bottom=true)
         setContentView(binding.root)
 
-        // Re-set header text with the app name
+        // Re-set header text & hide launcher checkbox with the app name
         binding.welcomeHeader.text = getString(R.string.welcome_header, getString(R.string.app_name))
-        // Register continue button click listener
+        binding.welcomeHideLauncher.text = getString(R.string.welcome_hide_launcher, getString(R.string.app_name))
+        // Register continue click listener
         binding.welcomeContinue.setOnClickListener {
-            disableOnDestroy = true
+            if (binding.welcomeHideLauncher.isChecked) {
+                disableOnDestroy = true
+            }
+            preferences.edit {
+                putBoolean("welcome_hide_launcher", binding.welcomeHideLauncher.isChecked)
+                apply()
+            }
+
             finish()
         }
     }
