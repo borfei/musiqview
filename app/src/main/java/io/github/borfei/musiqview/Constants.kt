62b30f04315732c265824ff325e1c4b7103b2390
feivegian <feivegian@gmail.com>
2025-02-05 20:11:21 +0800

    feat: Remove audio playback caching
    This commit removes the playback caching feature.

--- app/src/main/java/io/github/borfei/musiqview/Constants.kt
+++ app/src/main/java/io/github/borfei/musiqview/Constants.kt
@@ -5,7 +5,6 @@ object Constants {
     const val PREFERENCE_PLAYBACK_AUDIO_FOCUS = "playback_audio_focus"
     const val PREFERENCE_PLAYBACK_CONSTANT_BITRATE_SEEKING = "playback_constant_bitrate_seeking"
     const val PREFERENCE_PLAYBACK_DURATION_INTERVAL = "playback_duration_interval"
-    const val PREFERENCE_PLAYBACK_MAX_CACHE_SIZE = "playback_max_cache_size"
     const val PREFERENCE_OTHER_ANIMATE_LAYOUT_CHANGES = "other_animate_layout_changes"
     const val PREFERENCE_OTHER_WAKE_LOCK = "other_wake_lock"
     const val PREFERENCE_OTHER_IMMERSIVE_MODE = "other_immersive_mode"
