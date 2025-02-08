dcdfa2263368a53f8cf0eeee9c8b4ebbe82c803a
borfei <feivegian@gmail.com>
2025-02-08 13:12:37 +0800

    feat: Hide `LauncherActivity` after finishing set-up

--- app/src/main/java/io/github/borfei/musiqview/LauncherActivity.kt
+++ app/src/main/java/io/github/borfei/musiqview/LauncherActivity.kt
@@ -3,12 +3,19 @@ package io.github.borfei.musiqview
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import io.github.borfei.musiqview.databinding.ActivityLauncherBinding
 import io.github.borfei.musiqview.extensions.adjustPaddingForSystemBarInsets
+import io.github.borfei.musiqview.extensions.isActivityEnabled
+import io.github.borfei.musiqview.extensions.setActivityEnabled
 
 class LauncherActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "LauncherActivity"
+    }
+
     private lateinit var binding: ActivityLauncherBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +34,11 @@ class LauncherActivity : AppCompatActivity() {
             startActivity(Intent(Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS))
         }
         binding.launcherFinish.setOnClickListener {
+            if (packageManager.isActivityEnabled(this)) {
+                packageManager.setActivityEnabled(this, false)
+                Log.d(TAG, "This activity can no longer be accessible.")
+            }
+
             finish()
         }
     }
