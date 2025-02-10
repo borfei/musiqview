862ad72ed3d3bd5475cbc25d9a1bb35c0197845f
borfei <feivegian@gmail.com>
2025-02-10 11:45:58 +0800

    feat: Improve media information parsing

--- app/src/main/java/io/github/borfei/musiqview/ui/MediaInfoAlertDialog.kt
+++ app/src/main/java/io/github/borfei/musiqview/ui/MediaInfoAlertDialog.kt
@@ -2,27 +2,23 @@ package io.github.borfei.musiqview.ui
 
 import android.annotation.SuppressLint
 import android.app.Dialog
-import android.net.Uri
 import android.os.Bundle
-import androidx.annotation.OptIn
 import androidx.fragment.app.DialogFragment
-import androidx.media3.common.MediaMetadata
-import androidx.media3.common.util.UnstableApi
+import androidx.media3.common.C
+import androidx.media3.session.MediaController
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import io.github.borfei.musiqview.R
 import io.github.borfei.musiqview.databinding.AlertDialogMediaInfoBinding
 import io.github.borfei.musiqview.extensions.getName
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
-class MediaInfoAlertDialog(
-    private val mediaMetadata: MediaMetadata,
-    private val mediaUri: Uri
-) : DialogFragment() {
+class MediaInfoAlertDialog(private val mediaController: MediaController) : DialogFragment() {
     companion object {
         const val TAG = "MediaInfoAlertDialog"
     }
 
     @SuppressLint("SetTextI18n")
-    @OptIn(UnstableApi::class)
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val binding = AlertDialogMediaInfoBinding.inflate(layoutInflater)
         val dialog = MaterialAlertDialogBuilder(requireContext())
@@ -31,69 +27,37 @@ class MediaInfoAlertDialog(
             .setNegativeButton(R.string.dialog_media_info_negative) { it, _ -> it.dismiss() }
             .create()
 
-        mediaMetadata.title?.let {
-            if (it.isNotEmpty()) {
-                binding.mediaInfoTitle.text = it
-            }
-        }
-        mediaMetadata.displayTitle?.let {
-            if (it.isNotEmpty()) {
-                binding.mediaInfoDisplayTitle.text = it
-            }
-        }
-        mediaMetadata.artist?.let {
-            if (it.isNotEmpty()) {
-                binding.mediaInfoArtist.text = it
-            }
-        }
-        mediaMetadata.writer?.let {
-            if (it.isNotEmpty()) {
-                binding.mediaInfoWriter.text = it
-            }
-        }
-        mediaMetadata.composer?.let {
-            if (it.isNotEmpty()) {
-                binding.mediaInfoComposer.text = it
-            }
+        val mediaMetadata = mediaController.mediaMetadata
+        val mediaUri = mediaController.currentMediaItem?.localConfiguration?.uri
+
+        mediaMetadata.let { meta ->
+            meta.title?.let { binding.mediaInfoTitle.text = it }
+            meta.displayTitle?.let { binding.mediaInfoDisplayTitle.text = it }
+            meta.artist?.let { binding.mediaInfoArtist.text = it }
+            meta.writer?.let { binding.mediaInfoWriter.text = it }
+            meta.composer?.let { binding.mediaInfoComposer.text = it }
+
+            meta.albumTitle?.let { binding.mediaInfoAlbum.text = it }
+            meta.albumArtist?.let { binding.mediaInfoAlbumArtist.text = it }
+            meta.discNumber?.let { binding.mediaInfoDiscNumber.text = it.toString() }
+            meta.trackNumber?.let { binding.mediaInfoTrackNumber.text = it.toString() }
+            meta.totalDiscCount?.let { binding.mediaInfoTotalDiscs.text = it.toString() }
+            meta.totalTrackCount?.let { binding.mediaInfoTotalTracks.text = it.toString() }
         }
 
-        mediaMetadata.albumTitle?.let {
-            if (it.isNotEmpty()) {
-                binding.mediaInfoAlbum.text = it
+        mediaController.duration.let {
+            binding.mediaInfoDuration.text = if (it != C.TIME_UNSET) {
+                it.toDuration(DurationUnit.MILLISECONDS).toComponents { minutes, seconds, _ ->
+                    getString(R.string.playback_seek_text_format, minutes, seconds)
+                }
+            } else {
+                getString(R.string.media_info_entry_placeholder)
             }
         }
-        mediaMetadata.albumArtist?.let {
-            if (it.isNotEmpty()) {
-                binding.mediaInfoAlbumArtist.text = it
-            }
-        }
-        mediaMetadata.discNumber?.let {
-            if (it > 0) {
-                binding.mediaInfoDiscNumber.text = it.toString()
-            }
-        }
-        mediaMetadata.trackNumber?.let {
-            if (it > 0) {
-                binding.mediaInfoTrackNumber.text = it.toString()
-            }
-        }
-        mediaMetadata.totalDiscCount?.let {
-            if (it > 0) {
-                binding.mediaInfoTotalDiscs.text = it.toString()
-            }
-        }
-        mediaMetadata.totalTrackCount?.let {
-            if (it > 0) {
-                binding.mediaInfoTotalTracks.text = it.toString()
-            }
+        mediaUri?.getName(requireContext())?.let {
+            binding.mediaInfoFilename.text = it
         }
 
-        mediaMetadata.durationMs?.let {
-            if (it > 0) {
-                binding.mediaInfoDuration.text = it.toString()
-            }
-        }
-        binding.mediaInfoFilename.text = mediaUri.getName(requireContext())
         return dialog
     }
 }
