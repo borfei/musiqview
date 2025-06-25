285eb9e5b8fc303d40528c475cc8b7c1884e159f
borgirsteek <184122023+borgirsteek@users.noreply.github.com>
2025-06-26 02:47:38 +0800

    Bump version to 3.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.feivegian.musicview"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "3.5.0"
+        versionCode = 7
+        versionName = "3.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
