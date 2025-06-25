4fc4cd8d6959e9991f6bb2ea2bd0ad6433f31f22
borgirsteek <184122023+borgirsteek@users.noreply.github.com>
2025-06-26 02:47:26 +0800

    docs: Add comment for getName function for Uri extension class

--- app/src/main/java/io/github/feivegian/musicview/extensions/Uri.kt
+++ app/src/main/java/io/github/feivegian/musicview/extensions/Uri.kt
@@ -4,6 +4,13 @@ import android.content.Context
 import android.net.Uri
 import android.provider.OpenableColumns
 
+/**
+ * Returns the user-friendly name for [Uri]
+ *
+ *
+ * @param[context] The context to be parsed the [Uri] with. Must not be null
+ * @return[String]
+ */
 fun Uri.getName(context: Context): String? {
     val returnCursor = context.contentResolver.query(this, null, null, null, null)
     val nameIndex = returnCursor?.getColumnIndex(OpenableColumns.DISPLAY_NAME)
